a03d92dcd7791dd4bbea57c2d59fc701384bbad4
MorenaMastromarino <morena.mastromarino98@gmail.com>
2021-11-09 16:43:35 +0100

    setTimeout

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 // testo iniziale
-document.getElementById('text').innerHTML='Hai 30 secondi per memorizzare i seguenti numeri:'
-document.querySelector('.box').innerHTML = generateNumbers(5);
+const randomNumbers = [];
+document.getElementById('text').innerHTML='Hai 30 secondi per memorizzare i seguenti numeri:';
+document.querySelector('.box').innerHTML = `<span>${generateNumbers(5)}</span>`;
+
+setTimeout(fineTimeout, 3000);   //cambiarlo in 30000
 
 
+// funzione che gestisce eventi dopo 30 sec
+function fineTimeout(){
+  document.querySelector('.box').innerHTML = '';
+  const numeriUtente = [];
 
+  for(let i = 0; i < 5; i++ ){
+    const numeroUtente = parseInt(prompt('Indovina i numeri'));
+    numeriUtente.push(numeroUtente);
+  }
 
+  console.log(randomNumbers);
+  console.log(numeriUtente);
+}
 
 // funzione che genera una stringa di cinque numeri diversi tra loro
-function generateNumbers(n) {
-  const randomNumbers = [];
+function generateNumbers(n) { 
   
   while(randomNumbers.length < n){
     const number = getRndInteger(1, 99);
